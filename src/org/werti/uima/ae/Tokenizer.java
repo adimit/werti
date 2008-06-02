53429b275a9ba6ce9a6b75c0ef73a9ff41e17e97
Aleksandar L. Dimitrov <aleks@brmbr.sfs.uni-tuebingen.de>
2008-06-03 01:22:56 +0200

    Revert "Added testcases, tokenizer works."
    This reverts commit 2ce9a0a7534bb2bb1a13a1c0c3cbfa097abebe3c.
    
    I had a small git-panic. This tool is very powerful, but thankfully
    VCS makes it hard to shoot yourself in the foot if you don't just
    rm -rf ./.git. I'll commit this now, although I didn't really make
    changes. Just for safety reasons. Project should be on the newest
    state right now.
    
    Good thing I have copies and backups of everything :-)

---
+++ src/org/werti/uima/ae/Tokenizer.java
@@ -0,0 +1,57 @@
+package org.werti.uima.ae;
+
+import java.io.StringReader;
+
+import java.util.Iterator;
+
+import edu.stanford.nlp.ling.Word;
+
+import edu.stanford.nlp.process.PTBTokenizer;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+
+import org.apache.uima.cas.FSIndex;
+
+import org.apache.uima.jcas.JCas;
+
+import org.apache.uima.util.Level;
+
+import org.werti.uima.types.annot.RelevantText;
+import org.werti.uima.types.annot.Token;
+
+public class Tokenizer extends JCasAnnotator_ImplBase {
+
+	/**
+	 * Go through all relevant text areas in the RelevantText-AnnotationIndex and annotate them
+	 * with Token-Annotations.
+	 *
+	 * We only set the CAS-relative Begin and End, but also set the Word-Property of Token for now. This may
+	 * change in the future for efficiency reasons.
+	 */
+	public void process(JCas cas) {
+		final FSIndex textIndex = cas.getAnnotationIndex(RelevantText.type);
+		final Iterator<RelevantText> tit = textIndex.iterator();
+
+		while (tit.hasNext()) {
+			final RelevantText rt = tit.next();
+			final String span = rt.getCoveredText();
+			final PTBTokenizer<Word> tokzr = PTBTokenizer.newPTBTokenizer(new StringReader(span), false);
+
+			// global and local skews
+			final int gskew = rt.getBegin();
+			int lskew = 0;
+
+			tokenizeSpan: while (tokzr.hasNext()) {
+				final Word w = tokzr.next();
+				final int index = span.indexOf(w.word(), lskew);
+				if (index == -1) continue tokenizeSpan;
+				else lskew = index;
+				final Token t = new Token(cas);
+				t.setBegin(lskew + gskew);
+				t.setEnd((lskew += w.word().length()) + gskew);
+				t.setWord(w.word());
+				t.addToIndexes();
+			}
+		}
+	}
+}
---
+++ src/org/werti/uima/ae/relevance/GenericRelevanceAnnotator.java
@@ -0,0 +1,56 @@
+package org.werti.uima.ae.relevance;
+
+import java.util.Iterator;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+
+import org.apache.uima.cas.FSIndex;
+
+import org.apache.uima.jcas.JCas;
+
+import org.apache.uima.util.Level;
+
+import org.werti.uima.types.annot.HTML;
+import org.werti.uima.types.annot.RelevantText;
+
+/**
+ * Generic Relevance annotator.
+ *
+ * Looks at the HTML tags of a document and determines what would be suitable input 
+ * for the PoS tagger and the input enhancer.
+ *
+ * Sometimes this will mark garbage. It might prove hard to actually fix this, though.
+ */
+public class GenericRelevanceAnnotator extends JCasAnnotator_ImplBase {
+
+	private static final int RELEVANCE_TRESHOLD = 30;
+	public void process(JCas cas) {
+		final FSIndex tagIndex = cas.getAnnotationIndex(HTML.type);
+		final Iterator<HTML> tit = tagIndex.iterator();
+		HTML tag = tit.next();
+		if (tag == null) {
+			getContext().getLogger().log(Level.SEVERE, 
+					"HTML tag was null while starting to search for relevance.");
+		}
+		boolean inBody = false;
+		titerator: while (tit.hasNext()){
+			final int t = tag.getEnd();
+			final String tname = tag.getTag_name();
+			if (!inBody) {
+				if (tname.equals("body")
+				||  tname.equals("/head")) {
+					inBody = true;
+				}
+				tit.next();
+				continue titerator;
+			}
+			if (RELEVANCE_TRESHOLD < ((tag = tit.next()).getBegin() - t)) {
+				final RelevantText rt = new RelevantText(cas);
+				rt.setBegin(t);
+				rt.setEnd(tag.getBegin());
+				rt.setEnclosing_tag(tag.getTag_name());
+				rt.addToIndexes();
+			}
+		}
+	}
+}
