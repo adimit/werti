3e2063e6798d2279a1363b6e43c9b7eaff08f7a5
Aleksandar L. Dimitrov <aleks_d@gmx.de>
2008-06-09 21:57:05 +0200

    Switched to new interface.
    While I was at it, I ditched RequestURL and Dispatcher.
    Interface.process() is still under a screenful and this way it really
    seems more consistent. We'd still need something to account for term
    queries. Google SOAP comes to my mind.
    
    For now, WERTi is back up again. I have no intention of making further
    changes to this basic infrastructure. So the system should stay up from
    now on.
    
    On top of my TODO list: let the user choose a tag set. Implement other
    enhancement methods. Implement lingpipe tools.

--- src/org/werti/Interface.java
+++ src/org/werti/Interface.java
@@ -2,15 +2,34 @@ package org.werti;
 
 import java.io.*;
 
+import java.util.Iterator;
 import java.util.Map;
 
 import java.util.logging.*;
+import java.util.logging.Level;
 
 import javax.servlet.*;
 
 import javax.servlet.http.*;
 
-import org.werti.uima.UnrecoverableUIMAException;
+import lib.html.Net;
+
+import org.apache.uima.UIMAFramework;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+
+import org.apache.uima.cas.FSIndex;
+
+import org.apache.uima.jcas.JCas;
+
+import org.apache.uima.jcas.cas.IntegerArray;
+import org.apache.uima.jcas.cas.StringArray;
+
+import org.apache.uima.resource.ResourceSpecifier;
+
+import org.apache.uima.util.XMLInputSource;
+
+import org.werti.uima.types.Enhancement;
 
 /**
  * It will, according to its arguments, call the appropriate classes that
@@ -19,19 +38,27 @@ import org.werti.uima.UnrecoverableUIMAException;
 
 public class Interface extends HttpServlet {
 	static final long serialVersionUID = 0;
+	private static final String PREFIX = "/home/aleks/src/werti/desc/operators/";
 
 	private static final Logger log = Logger.getLogger("org.werti");
 
 	@SuppressWarnings("unchecked")
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws IOException, ServletException {
+		//DEBUG
+		log.setLevel(Level.ALL);
+
+		// Get parameters
 		final String[] tags = request.getParameter("tags").split("\\s*,\\ss*");
 		final String tagger = request.getParameter("tagger");
 		final String termOrURL = request.getParameter("termOrUrl");
 		final String tokenizer = request.getParameter("tokenizer");
 		final String enhance = request.getParameter("enhance");
 
-		log.setLevel(Level.ALL);
+		final String baseurl = Net.find_baseurl(termOrURL);
+		log.fine("Fetching: " + termOrURL + "; baseurl = " + baseurl);
+
+		// log this event's arguments
 		final Map<String,String[]> args = request.getParameterMap();
 		for (final Map.Entry<String,String[]> e: args.entrySet()) {
 		     final String[] vs = e.getValue();
@@ -41,20 +68,90 @@ public class Interface extends HttpServlet {
 		     }
 		     log.config(e.getKey() + " = " + r);
 		}
+
+		final String descriptor = PREFIX + tokenizer + "-" + tagger + "-hil.xml";
+		log.config("Calling descriptor " + descriptor);
+
+		final PrintWriter out = response.getWriter();
+		final BufferedReader in = Net.fetch(termOrURL);
+
+		if (!in.ready()) {
+			log.severe("Input stream to " + termOrURL + " is broken!");
+		}
+
+		try {
+			final XMLInputSource xmlin = new XMLInputSource(descriptor);
+			final ResourceSpecifier spec = UIMAFramework.getXMLParser().parseResourceSpecifier(xmlin);
+			final AnalysisEngine ae  = UIMAFramework.produceAnalysisEngine(spec);
+			final JCas cas = ae.newJCas();
+
+			cas.setDocumentText(bis2str(in));
+			ae.process(cas);
+
+			final String enhanced = enhance(cas, baseurl);
+			out.print(enhanced);
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Processing encountered errors!", e);
+		}
+
+
+		response.setContentType("text/html");
+
 	}
 
-	private static String getTokenizer(String tok) throws UnrecoverableUIMAException {
-		if (tok.equals("ptb")) {
-			return "desc/annotators/PTBTokenizer.xml";
-		} else if (tok.equals("lgp")) {
-			return "desc/annotators/LingPipeTokenizer.xml";
-		} else if (tok.equals("own")) {
-			return "desc/annotators/SimpleTokenizer.xml";
-		} else throw new UnrecoverableUIMAException("Unkonwn tokenizer: " + tok);
+	private static String bis2str(BufferedReader in) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			while (in.ready()) {
+				sb.append(in.readLine());
+			}
+		} catch (IOException ioe) {
+			log.log(Level.SEVERE, "Encountered errors while retrieving remote site!", ioe);
+		}
+		return sb.toString();
 	}
 
-	private static String getTagger(String tag) {
-		return null;
+	/** 
+	 * Input-Enhances the CAS given with Enhancement annotations.
+	 *
+	 * This is an easy to understand version of enhance.
+	 *
+	 * @param cas The cas to enhance.
+	 */
+	@SuppressWarnings("unchecked")
+	private String enhance(JCas cas, String baseurl) {
+		final String docText = cas.getDocumentText();
+		final StringBuilder rtext = new StringBuilder(docText);
+
+		int skew = docText.indexOf("<head");
+		skew = docText.indexOf('>',skew)+1;
+
+		final String basetag = "<base href=\"http://" + baseurl + "\" />";
+		rtext.insert(skew, basetag);
+		skew = basetag.length();
+
+		final FSIndex tagIndex = cas.getAnnotationIndex(Enhancement.type);
+		final Iterator<Enhancement> eit = tagIndex.iterator();
+
+		while (eit.hasNext()) {
+			final Enhancement e = eit.next();
+			final StringArray sa = e.getEnhancement_list();
+			final IntegerArray ia = e.getIndex_list();
+			if (sa == null || ia == null) {
+				log.severe("Found no eList or iList on Enhancement");
+				continue;
+			}
+			assert sa.size() == ia.size();
+			for (int p = 0; p < sa.size(); p++) {
+				final String s = sa.get(p);
+				final int i = ia.get(p)+skew;
+
+				rtext.insert(i, s);
+
+				skew += s.length();
+			}
+		}
+		return rtext.toString();
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
