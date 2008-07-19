b14cf0dd93fac7c7d8b5875fd9b4d2dda083dd29
Александър Димитров <aleks_d@gmx.de>
2008-07-19 04:15:36 +0200

    Improved Cloze Item handling.
    We now depend on the external Composite ClozeItem. Help button not
    worknig yet, because of that <p><div>blah</div></p> issue. Meh.

--- src/org/werti/enhancements/client/Cloze.java
+++ src/org/werti/enhancements/client/Cloze.java
@@ -9,12 +9,8 @@ import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FocusListener;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -22,75 +18,6 @@ import com.google.gwt.user.client.ui.Widget;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Cloze implements EntryPoint {
-
-	private class ClozeListener implements KeyboardListener, ClickListener, FocusListener {
-		final String gottagetit;
-		final Element elem;
-		final TextBox current, nextBox;
-
-		public ClozeListener(Element elem, TextBox nextBox, TextBox current) {
-			this.gottagetit = elem.getInnerText();
-			elem.setInnerText("");
-			this.elem = elem;
-			this.current = current;
-			this.nextBox = nextBox;
-		}
-
-		public void onKeyDown(Widget w, char code, int mods) { }
-		
-		/**
-		 * When the user presses "enter", we check and reat.
-		 *
-		 * If it's correct, we also move the focus to the next item.
-		 */
-		public void onKeyPress (Widget sender, char keyCode, int modifiers) {
-			if (keyCode == KeyboardListener.KEY_ENTER) {
-				if (sender instanceof TextBox) {
-					final TextBox tb = (TextBox) sender;
-					if (tb.getText().equals(gottagetit)) {
-						addSuccess();
-						tb.setStyleName("WERTiClozeTextWin");
-						tb.setEnabled(false);
-						nextBox.setFocus(true);
-					} else {
-						addFault();
-						tb.setStyleName("WERTiClozeTextFail");
-					}
-				} else {
-					showError("Someone sent ClozeListener an event, "
-						+ "but we don't know who he is! He says "
-						+ "he's " + sender.getClass());
-				}
-			}
-		}
-
-		public void onKeyUp (Widget sender, char keyCode, int modifiers) { }
-
-		public void onLostFocus(Widget w) { }
-
-		/**
-		 * If we aren't enabled, pass the buck, the user can't do anything.
-		 */
-		public void onFocus(Widget w) {
-			if (!current.isEnabled()) {
-				nextBox.setFocus(true);
-			}
-		}
-
-		/**
-		 * The user requested help and we set the textbox to "I have helped you" and
-		 * put the target text in it.
-		 */
-		public void onClick(Widget w) {
-			current.setStyleName("WERTiClozeTextHelped");
-			current.setText(gottagetit);
-			current.setEnabled(false);
-		}
-	}
-
-	public void addFault() { }
-
-	public void addSuccess() { }
 
 	/**
 	 * Inform the user when something didn't work out the way we expected to.
@@ -122,49 +49,23 @@ public class Cloze implements EntryPoint {
 
 	/**
 	 * This is the entry point method.
+	 *
+	 * We make a linked list out of the text boxes, so we can forward focus.
 	 */
 	public void onModuleLoad() {
 		Element domSpan;
-		ClozeItem tb0, tb1;
-		tb0 = tb1 = new ClozeItem("WERTiClozeText");
+		ClozeItem tb0;
 		int i = 1;
+		tb0 = new ClozeItem(RootPanel.get(getId(i)));
 		while ((domSpan = DOM.getElementById(getId(i))) != null) {
-			tb0.setStyleName("WERTiClozeText");
-			tb0.addListeners(domSpan, tb1);
-			RootPanel.get(getId(i)).add(tb0);
-			tb0 = tb1; tb1 = new ClozeItem("WERTiClozeText");
-			i++;
+			tb0.setTarget(domSpan.getInnerText());
+			domSpan.setInnerText("");
+			tb0.setNext(new ClozeItem(RootPanel.get(getId(i++))));
+			tb0 = tb0.getNext();
 		}
 	}
 
 	private static String getId(int id) {
 		return "WERTi-span-" + id;
-	}
-
-	private class ClozeItem extends HorizontalPanel {
-		final TextBox target;
-		final Button  helpButton;
-
-		public ClozeItem(final String style) {
-			super();
-			this.setStyleName("WERTiClozeItem");
-			target = new TextBox();
-			target.setStyleName(style);
-			helpButton = new Button("?");
-			helpButton.setStyleName("WERTiHelpButton");
-			this.add(target);
-			this.add(helpButton);
-		}
-
-		public void addListeners(Element elem, ClozeItem next) {
-			final ClozeListener heinzelmann = new ClozeListener(elem, next.target, target);
-			target.addKeyboardListener(heinzelmann);
-			target.addFocusLilstener(heinzelmann);
-			helpButton.addClickListener(heinzelmann);
-		}
-
-		public void setText(String t) {
-			target.setText(t);
-		}
 	}
 }
---
+++ src/org/werti/enhancements/client/ClozeItem.java
@@ -0,0 +1,184 @@
+package org.werti.enhancements.client;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FocusListener;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.KeyboardListener;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SourcesChangeEvents;
+import com.google.gwt.user.client.ui.SourcesClickEvents;
+import com.google.gwt.user.client.ui.SourcesKeyboardEvents;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ClozeItem
+		implements HasText
+			   , SourcesClickEvents
+			   , SourcesKeyboardEvents
+			   , SourcesChangeEvents {
+	private String target;
+	private ClozeItem next;
+	private final TextBox item;
+	private final Button help;
+	private static int fails, wins, helps = 0;
+
+	public ClozeItem(final RootPanel root) {
+		this.item = new TextBox();
+		item.addKeyboardListener(new KeyboardListener() {
+			public void onKeyPress(Widget sender, char keyCode, int mods) {
+				if (keyCode == KeyboardListener.KEY_ENTER) {
+					if (item.getText().equals(target)) {
+						item.setStyleName("WERTiClozeTextWin");
+						item.setEnabled(false);
+						wins++;
+						next.item.setFocus(true);
+					} 
+					else {
+						item.setStyleName("WERTiClozeTextFail");
+						fails++;
+					}
+				}
+			}
+			public void onKeyDown(Widget w, char c, int i) { }
+			public void onKeyUp(Widget w, char c, int i) { }
+		});
+		item.setStyleName("WERTiClozeText");
+		item.addFocusListener(new FocusListener() {
+			public void onFocus(Widget w) {
+				if (!item.isEnabled()) {
+					next.item.setFocus(true);
+				}
+			}
+			public void onLostFocus(Widget w) { }
+		});
+		this.help = new Button("?");
+		help.addClickListener(new ClickListener() {
+			public void onClick(Widget sender) {
+				item.setStyleName("WERTiClozeTextHelped");
+				item.setText(target);
+				item.setEnabled(false);
+				helps++;
+			}
+		});
+		help.setStyleName("WERTiHelpButton");
+
+		root.add(item);
+		//root.add(help);
+	}
+
+	public ClozeItem(RootPanel root, final String target, final ClozeItem next) {
+		this(root);
+		this.target = target;
+		this.next = next;
+	}
+
+	/**
+	 * Forwards to the text box
+	 *
+	 * @return The TextBox's <code>getText()</code>
+	 */
+	public String getText() {
+		return this.item.getText();
+	}
+
+	/**
+	 * Forwards to the text box
+	 *
+	 * @param t What the TextBox should display
+	 */
+	public void setText(String t) {
+		this.item.setText(t);
+	}
+
+	/**
+	 * Forwards to the Button
+	 *
+	 * @param listener A listener to add to the Button
+	 */
+	public void addClickListener(ClickListener listener) {
+		this.help.addClickListener(listener);
+	}
+
+	/**
+	 * Forwards to the Button
+	 *
+	 * @param listener A listener to remove from the Button
+	 */
+	public void removeClickListener(ClickListener listener) {
+		this.help.removeClickListener(listener);
+	}
+
+	/**
+	 * Forwards to the TextBox
+	 *
+	 * @param listener A listener to add to the TextBox
+	 */
+	public void addChangeListener(ChangeListener l) {
+		this.item.addChangeListener(l);
+	}
+
+	/**
+	 * Forwards to the TextBox
+	 *
+	 * @param listener A listener to remove from the TextBox
+	 */
+	public void removeChangeListener(ChangeListener l) {
+		this.item.removeChangeListener(l);
+	}
+
+	/**
+	 * Forwards to the TextBox
+	 *
+	 * @param listener A listener to add to the TextBox
+	 */
+	public void addKeyboardListener(KeyboardListener l) {
+		this.item.addKeyboardListener(l);
+	}
+
+	/**
+	 * Forwards to the TextBox
+	 *
+	 * @param listener A listener to remove from the TextBox
+	 */
+	public void removeKeyboardListener(KeyboardListener l) {
+		this.item.removeKeyboardListener(l);
+	}
+
+	/**
+	 * Gets the target for this instance. (i.e. the String this cloze item listens for)
+	 *
+	 * @return The target.
+	 */
+	public String getTarget () {
+		return this.target;
+	}
+
+	/**
+	 * Sets the pointer to the next cloze item for this instance.
+	 *
+	 * @param next The next.
+	 */
+	public void setNext (ClozeItem next) {
+		this.next = next;
+	}
+
+	/**
+	 * Sets the target for this instance. (i.e. the String this cloze item listens for)
+	 *
+	 * @param target The target.
+	 */
+	public void setTarget (String target) {
+		this.target = target;
+	}
+
+	/**
+	 * Gets the pointer to the next cloze item for this instance.
+	 *
+	 * @return The next.
+	 */
+	public ClozeItem getNext () {
+		return this.next;
+	}
+}
