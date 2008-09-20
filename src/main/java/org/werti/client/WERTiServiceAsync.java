2e5cbe723f15e383e5e4fde85e83debff8400046
Ramon Ziai <rziai@sfs.uni-tuebingen.de>
2008-09-20 21:15:12 +0200

    Changed SearchCallback to implement AsyncCallback<String> instead of raw type.

--- src/main/java/org/werti/client/WERTiServiceAsync.java
+++ src/main/java/org/werti/client/WERTiServiceAsync.java
@@ -1,17 +1,15 @@
 package org.werti.client;
 
+import org.werti.client.run.RunConfiguration;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-import org.werti.client.run.RunConfiguration;
-
 /**
- * The asynchronous service interface to the server.
+ * The service interface to the server.
  *
  * @author Aleksandar Dimitrov
  * @version 0.1
  */
 public interface WERTiServiceAsync {
-
 	/**
 	 * Process the user's request.
 	 *
@@ -19,9 +17,6 @@ public interface WERTiServiceAsync {
 	 * @param language The language the user says the target is in.
 	 * @param tags A bunch of part-of-speech tags you want to highlight.
 	 * @param url The URL of the page the user has requested.
-	 * @param callback The callback object of this call.
 	 */
-	@SuppressWarnings("unchecked")
-	public void process(RunConfiguration config, String url, AsyncCallback callback); 
-		//throws ProcessingException, InitializationException, URLException;
+	public void process(RunConfiguration config, String url, AsyncCallback<String> callback);
 }
