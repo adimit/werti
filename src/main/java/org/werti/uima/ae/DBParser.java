740d4023504034adad9ba23ac0434af73d6b7bbf
Александър Димитров <aleks_d@gmx.de>
2008-09-29 13:52:30 +0200

    Parser stub.

--- src/main/java/org/werti/uima/ae/DBParser.java
+++ src/main/java/org/werti/uima/ae/DBParser.java
@@ -1,5 +1,10 @@
 package org.werti.uima.ae;
 
+import danbikel.parser.DecoderServer;
+import danbikel.parser.Parser;
+
+import org.apache.log4j.Logger;
+
 import org.apache.uima.UimaContext;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
@@ -9,12 +14,23 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
 public class DBParser extends JCasAnnotator_ImplBase {
+	private static final Logger log =
+		Logger.getLogger(DBParser.class);
+
+
+	private static Parser parser;
+	private DecoderServer server;
+
 	public void initialize(UimaContext context) throws ResourceInitializationException {
-	
+		try {
+			parser = new Parser("/home/aleks/var/tmp/dbparser/wsj-02-21.obj.gz");
+		} catch (Exception e) {
+			log.fatal(e);
+		}
 	}
 
 	public void process(JCas cas) {
-	
+
 	}
 
 }
