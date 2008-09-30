e190d0e055929387869428883a6d0321857ad0ff
Александър Димитров <aleks_d@gmx.de>
2008-09-30 14:21:43 +0200

    Added multi-word triggers.

--- src/main/java/org/werti/uima/ae/ConditionalAnnotator.java
+++ src/main/java/org/werti/uima/ae/ConditionalAnnotator.java
@@ -20,25 +20,51 @@ public class ConditionalAnnotator extends JCasAnnotator_ImplBase {
 	{ "if", "unless", "given", "suppose", "provided", "supposing", "providing" };
 	private static final List<String> triggers = new ArrayList<String>(Arrays.asList(triggersa));
 
+	private static final String[] mwtriggersa =
+	{ "solongas", "incase", "oncondition", "intheeventthat" };
+
+
 	@SuppressWarnings("unchecked")
 	public void process(JCas cas) {
 		final AnnotationIndex sindex = cas.getAnnotationIndex(SentenceAnnotation.type);
 		final AnnotationIndex tindex = cas.getAnnotationIndex(Token.type);
 
 		sentences: for(final Iterator<SentenceAnnotation> sit = sindex.iterator(); sit.hasNext(); ) {
+			final StringBuilder sb = new StringBuilder();
 			final SentenceAnnotation sentence = sit.next();
 			for(final Iterator<Token> tit = tindex.subiterator(sentence); tit.hasNext(); ) {
 				final Token t = tit.next();
-				final String s = t.getCoveredText();
+				final String s = t.getCoveredText().toLowerCase();
 				if (triggers.contains(s)) {
-					final Conditional c = new Conditional(cas);
-					c.setBegin(sentence.getBegin());
-					c.setEnd(sentence.getEnd());
-					c.setTrigger(s);
-					c.addToIndexes();
+					createAnnotation(s, sentence, cas);
 					continue sentences;
+				}
+				sb.append(s);
+				boolean contains = false;
+				for (String mwt:mwtriggersa) {
+					if (mwt.startsWith(sb.toString())) {
+						if (mwt.equals(sb.toString())) {
+							createAnnotation(s, sentence, cas);
+							continue sentences;
+						} else {
+							contains = true;
+						}
+					}
+				}
+				if (!contains) {
+					sb.delete(0, sb.length());
 				}
 			}
 		}
+	}
+
+	private static final void createAnnotation(final String trigger
+			, final SentenceAnnotation sentence
+			, final JCas cas) {
+		final Conditional c = new Conditional(cas);
+		c.setBegin(sentence.getBegin());
+		c.setEnd(sentence.getEnd());
+		c.setTrigger(trigger);
+		c.addToIndexes();
 	}
 }
