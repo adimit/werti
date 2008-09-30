013f00c04dfe47c1d75c9406f947e83641934030
Александър Димитров <aleks_d@gmx.de>
2008-09-30 18:37:29 +0200

    Bugfixes, Server now starts its own Switchboard

--- src/main/java/org/werti/util/WERTiServer.java
+++ src/main/java/org/werti/util/WERTiServer.java
@@ -1,41 +1,47 @@
 package org.werti.util;
 
+import java.io.PrintWriter;
+
 import java.rmi.RemoteException;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import danbikel.parser.DecoderServer;
 import danbikel.parser.DecoderServerRemote;
-import danbikel.parser.Settings;
 
 import danbikel.switchboard.Switchboard;
 
-import org.apache.log4j.Logger;
-
 public class WERTiServer extends DecoderServer implements DecoderServerRemote {
-	private static final Logger log =
-		Logger.getLogger(WERTiServer.class);
 
 	private static final long serialVersionUID = 2;
 
-	private static final int PORT = 1099;
-	private static final int TIMEOUT = 5000;
+	private static final int TIMEOUT = 20;
 
 	private final static String derivedDataFilename = "src/main/resources/wsj-02-21.obj.gz";
 
 
 	public WERTiServer() throws RemoteException {
-		super(TIMEOUT, PORT);
+		super(TIMEOUT);
 	}
 
 	public static void main(String[] args) {
 		try {
+			final Switchboard sb = new Switchboard(new PrintWriter(System.out));
+			sb.export();
+			final Registry r = LocateRegistry.createRegistry(1099);
+			r.bind("Switchboard", sb);
+			System.err.printf("Constructed Switchboard\n");
 			final WERTiServer srv = new WERTiServer();
+			System.out.println("Registering with Switchboard now.");
+			srv.register("Switchboard");
+			System.out.println("I have the ID " + srv.id);
 			srv.setModelCollection(derivedDataFilename);
-			srv.register(Switchboard.defaultBindingName);
-			Settings.setSettings(srv.switchboard.getSettings());
+			System.err.println("Listening…");
 			srv.startAliveThread();
 			srv.unexportWhenDead();
 		} catch (Exception e) {
-			log.fatal(e);
+			e.printStackTrace(System.out);
 		}
 	}
 }
