5f374672e0266541c5861d543f9a2f7403558f33
Ramon Ziai <rziai@sfs.uni-tuebingen.de>
2008-10-11 16:21:34 +0200

    Trying to get GerundsAsk to work...

--- src/main/java/org/werti/enhancements/client/GerundsAsk.java
+++ src/main/java/org/werti/enhancements/client/GerundsAsk.java
@@ -2,7 +2,9 @@ package org.werti.enhancements.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class GerundsAsk implements EntryPoint {
 
@@ -14,61 +16,62 @@ public class GerundsAsk implements EntryPoint {
 
 	private static final String prefix = "WERTi-span";
 	private static final String gerColor = "#ce6006";
-	private final String infColor = "#8c01c0";
-	private final String cluColor = "#2222ff";
-	private final String ingformColor = "black";
-	private final String ambiguousColor = "red";
+	private static final String infColor = "#8c01c0";
+	private static final String cluColor = "#2222ff";
+	private static final String ingformColor = "black";
+	private static final String ambiguousColor = "red";
 	
 	private static final int maxParentLevel = 2;
 	
 	public void onModuleLoad() {
 		Element domSpan;
-		int i = 1;
-		// go through all tokens
-		while ((domSpan = DOM.getElementById(prefix + "-" + (i))) != null) {
-			Element ancestor = null;
-			if ((ancestor = getAncestor(domSpan, ".+CLU.+")) != null) {
-				final String text = domSpan.getInnerText();
-				final String a_start = "<a href=\"javascript:void(null)\" style=\"color:black\" "
+		for (int ii = 1; (domSpan = DOM.getElementById(prefix + "-" + RELEVANT + "-" + ii)) != null; ii++) {
+			NodeList<Element> spans = domSpan.getElementsByTagName("span");
+			for (int sp = 0; sp < spans.getLength(); sp++) {
+				Element span = spans.getItem(sp);
+				if (span.getId().contains("CLU")) {
+					String text = span.getInnerText();
+					final String a_start = 
+						"<a href=\"javascript:void(null)\" style=\"color:black\" "
 						+ "onclick=\"{this.style.color = 'green"
 						+ "'; this.style.fontWeight = 'bold';}\">";
-				domSpan.setInnerHTML(a_start + text + "</a>");
-			} else {
-				final String text = domSpan.getInnerText();
-				final String a_start = "<a href=\"javascript:void(null)\" style=\"color:black\" "
-						+ "onclick=\"{this.style.color = 'red"
-						+ "'; this.style.fontWeight = 'bold';}\">";
-				domSpan.setInnerHTML(a_start + text + "</a>");
+					span.setInnerHTML(a_start + text + "</a>");
+				} else if (span.getId().contains(GER)) {
+					span.setInnerHTML("<span style=\"color: "+ gerColor +"; font-weight:bold\">"
+							+ span.getInnerText() + "</span>");
+				} else if (span.getId().contains(INF)) {
+					span.setInnerHTML("<span style=\"color: "+ infColor +"; font-weight:bold\">"
+							+ span.getInnerText() + "</span>");
+				}
 			}
-			i++;
-		}
-		markHelper("GER", gerColor);
-		markHelper("INF", infColor);
-	}
-	
-	private static Element getAncestor(Element elem, String regex){
-		Element current = elem;
-		int i = 0;
-		while (i < maxParentLevel && (current = current.getParentElement()) != null) {
-			if (current.getId().matches(regex)) {
-				return current;
+			
+			spans = domSpan.getElementsByTagName("span");
+			for (int sp = 0; sp < spans.getLength(); sp++) {
+				Element span = spans.getItem(sp);
+				if (span.getId().matches(prefix + "-\\d+") && span.getFirstChildElement() != null) {
+					Element firstChild = span.getFirstChildElement();
+					if (firstChild.getTagName().equals("span")) {
+						Element spanParent = span.getParentElement();
+						spanParent.replaceChild(span, firstChild);
+					}
+				}
 			}
-			i++;
+			
 		}
-		return null;
-	}
-	
-	private void markHelper(String code, String color) {
-		int i = 1;
-		Element domSpan;
-		String pref = prefix + "-" + code + "-"; 
 		
-		// loop over all elements with the given ID and mark them
-		while ((domSpan = DOM.getElementById(pref + i)) != null) {
-			domSpan.setInnerHTML("<span style=\"color: "+ color +"; font-weight:bold\">"
-					+ domSpan.getInnerText() + "</span>");
-			i++;
+		NodeList<Element> remainingSpans = RootPanel.getBodyElement().getElementsByTagName("span");
+		for (int i = 0; i < remainingSpans.getLength(); i++) {
+			domSpan = remainingSpans.getItem(i);
+			if (domSpan.getId().matches(prefix + "-\\d+")) {
+				final String text = domSpan.getInnerText();
+				final String a_start = 
+					"<a href=\"javascript:void(null)\" style=\"color:black\" "
+					+ "onclick=\"{this.style.color = 'red"
+					+ "'; this.style.fontWeight = 'bold';}\">";
+				domSpan.setInnerHTML(a_start + text + "</a>");
+			}
 		}
+		
 	}
-
+	
 }
